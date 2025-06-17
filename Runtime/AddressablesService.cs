fb59de091c5c43d193f0d2d2076b1de611afbfc4
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-06-17 10:26:48 +0200

    code review changes

--- Runtime/AddressablesService.cs
+++ Runtime/AddressablesService.cs
@@ -52,6 +52,80 @@ namespace RossoForge.Addressables
             );
         }
 
+        public void Release(string address)
+        {
+            Release(_defaultContainerKey, address);
+        }
+
+        public void Release(string containerKey, string address)
+        {
+            if (!_handleMap.TryGetValue(containerKey, out var container))
+                throw new KeyNotFoundException($"Addressable container key '{containerKey}' not found.");
+
+            if (!container.TryGetValue(address, out var handler))
+                throw new KeyNotFoundException($"Addressable address '{address}' not found in container '{containerKey}'");
+
+            container.Remove(address);
+            UnityEngine.AddressableAssets.Addressables.Release(handler);
+
+#if UNITY_EDITOR
+            Debug.Log($"Addressable released: {address}");
+#endif
+        }
+        public void ReleaseAll()
+        {
+            var containers = _handleMap.Keys.ToList();
+            foreach (var container in containers)
+            {
+                ReleaseAll(container);
+            }
+            _handleMap.Clear();
+        }
+        public void ReleaseAll(string containerKey)
+        {
+            if (!_handleMap.TryGetValue(containerKey, out var container))
+                throw new KeyNotFoundException($"Addressable container key '{containerKey}' not found.");
+
+            foreach (var handle in container)
+            {
+                if (handle.Value.IsValid())
+                    UnityEngine.AddressableAssets.Addressables.Release(handle.Value);
+            }
+
+            _handleMap.Remove(containerKey);
+#if UNITY_EDITOR
+            Debug.Log($"Addressable container released: {containerKey}");
+#endif
+        }
+
+        private void RegisterHandle(string containerKey, string address, AsyncOperationHandle handle)
+        {
+            if (!_handleMap.ContainsKey(containerKey))
+                _handleMap[containerKey] = new Dictionary<string, AsyncOperationHandle>();
+
+            _handleMap[containerKey].TryAdd(address, handle);
+#if UNITY_EDITOR
+            Debug.Log($"Addressable loaded: {address}");
+#endif
+        }
+        private bool TryGetAddressable<T>(string containerKey, string address, out T result) where T : Object
+        {
+            result = default;
+
+            if (!_handleMap.TryGetValue(containerKey, out var container))
+                return false;
+
+            if (container.TryGetValue(address, out var handler))
+            {
+                if (handler.Result is not T)
+                    throw new System.InvalidCastException($"It is not possible to convert the addressable '{address}' of type {handler.Result.GetType().Name} to type {typeof(T).Name}");
+
+                result = handler.Result as T;
+                return true;
+            }
+
+            return false;
+        }
         private async Awaitable<T> LoadAsync<T>(string containerKey, string address, System.Func<AsyncOperationHandle<T>> loadAsset) where T : Object
         {
             if (TryGetAddressable<T>(containerKey, address, out var result))
@@ -82,84 +156,6 @@ namespace RossoForge.Addressables
             Debug.LogError($"Addressable load fail: {address}");
 #endif
             return null;
-        }
-
-        public void Release(string address)
-        {
-            Release(_defaultContainerKey, address);
-        }
-
-        public void Release(string containerKey, string address)
-        {
-            if (!_handleMap.TryGetValue(containerKey, out var container))
-                throw new KeyNotFoundException($"Addressable container key '{containerKey}' not found.");
-
-            if (!container.TryGetValue(address, out var handler))
-                throw new KeyNotFoundException($"Addressable address '{address}' not found in container '{containerKey}'");
-
-            container.Remove(address);
-            UnityEngine.AddressableAssets.Addressables.Release(handler);
-
-#if UNITY_EDITOR
-            Debug.Log($"Addressable released: {address}");
-#endif
-        }
-
-        public void ReleaseAll()
-        {
-            var containers = _handleMap.Keys.ToList();
-            foreach (var container in containers)
-            {
-                ReleaseAll(container);
-            }
-            _handleMap.Clear();
-        }
-
-        public void ReleaseAll(string containerKey)
-        {
-            if (!_handleMap.TryGetValue(containerKey, out var container))
-                throw new KeyNotFoundException($"Addressable container key '{containerKey}' not found.");
-
-            foreach (var handle in container)
-            {
-                if (handle.Value.IsValid())
-                    UnityEngine.AddressableAssets.Addressables.Release(handle.Value);
-            }
-
-            _handleMap.Remove(containerKey);
-#if UNITY_EDITOR
-            Debug.Log($"Addressable container released: {containerKey}");
-#endif
-        }
-
-        private void RegisterHandle(string containerKey, string address, AsyncOperationHandle handle)
-        {
-            if (!_handleMap.ContainsKey(containerKey))
-                _handleMap[containerKey] = new Dictionary<string, AsyncOperationHandle>();
-
-            _handleMap[containerKey].TryAdd(address, handle);
-#if UNITY_EDITOR
-            Debug.Log($"Addressable loaded: {address}");
-#endif
-        }
-
-        private bool TryGetAddressable<T>(string containerKey, string address, out T result) where T : Object
-        {
-            result = default;
-
-            if (!_handleMap.TryGetValue(containerKey, out var container))
-                return false;
-
-            if (container.TryGetValue(address, out var handler))
-            {
-                if (handler.Result is not T)
-                    throw new System.InvalidCastException($"It is not possible to convert the addressable '{address}' of type {handler.Result.GetType().Name} to type {typeof(T).Name}");
-
-                result = handler.Result as T;
-                return true;
-            }
-
-            return false;
         }
     }
 }
